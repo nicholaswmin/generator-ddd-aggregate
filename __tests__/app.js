3822503e58c69c1c024de8629de9d5a39e58a040
Nicholas Kyriakides <nicholaswmin@Nicholass-iMac.local>
2017-12-18 00:42:57 +0200

    fixed broken tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -12,24 +12,21 @@ describe('generator-ddd-aggregate:app', () => {
   });
 
   it('creates Class files', () => {
-    assert.file(['classes/social-user/index.js']);
+    assert.file(['social-user/classes/social-user/index.js']);
 
-    assert.file(['classes/social-user/test/index.js']);
-    assert.file(['classes/social-user/test/social-user.assertion.js']);
+    assert.file(['social-user/classes/social-user/test/index.js']);
+    assert.file(['social-user/classes/social-user/test/social-user.assertion.js']);
   });
 
-  it('creates Repo & DTG files', () => {
-    assert.file(['repos/dtgs/social-user-dtg/index.js']);
-    assert.file(['repos/social-user-repo/index.js']);
-
-    assert.file(['repos/social-user-repo/test/index.js']);
+  it('creates Repo files', () => {
+    assert.file(['social-user/repo/index.js']);
   });
 
   it('creates Service files', () => {
-    assert.file(['social-user-service/index.js']);
+    assert.file(['social-user/social-user-service/index.js']);
 
-    assert.file(['social-user-service/test/social-user-service.spec/index.js']);
-    assert.file(['social-user-service/test/index.js']);
-    assert.file(['social-user-service/test/setup.js']);
+    assert.file(['social-user/social-user-service/test/social-user-service.spec/index.js']);
+    assert.file(['social-user/social-user-service/test/index.js']);
+    assert.file(['social-user/social-user-service/test/setup.js']);
   });
 });
